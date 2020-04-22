de20a1155d3abb59ac563759741f0aeb630e6c68
yemimainadia <ratnasari.yemima@gmail.com>
2020-04-22 13:33:53 +0700

    styling responsive and custom js to detect mobile

---
+++ detect-browser.js
@@ -0,0 +1,7 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // some code..
+    console.log('mobile');
+
+} else {
+    console.log('desktop')
+}
